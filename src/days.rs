0d5113447d645245583572860f7d07b1c929c970
FilipSzutkowski <filip99.ski@gmail.com>
2023-09-12 07:22:54 +0200

    streamlined opening of input files

--- src/days.rs
+++ src/days.rs
@@ -1,5 +1,20 @@
 use crate::day1::main as day1;
 use crate::day2::main as day2;
+use std::fs::read_to_string;
+
+pub enum Days {
+    Day1,
+    Day2,
+}
+
+impl Days {
+    pub fn get_input(&self) -> String {
+        match self {
+            Days::Day1 => open_input_file("day1"),
+            Days::Day2 => open_input_file("day2"),
+        }
+    }
+}
 
 pub fn run_day(number: usize) {
     let all_days = [day1, day2];
@@ -16,6 +31,15 @@ pub fn parse_advent_day(input: &String) {
     match adv_day {
         Ok(adv_day) => run_day(adv_day),
         Err(_) => println!("Could not parse provided advent day."),
+    }
+}
+
+pub fn open_input_file(day: &str) -> String {
+    let file_path = format!("./inputs/{}", day);
+
+    match read_to_string(file_path) {
+        Ok(text) => text,
+        Err(err) => panic!("Panicked lolz /n{err}"),
     }
 }
 
